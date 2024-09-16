699dc49d8db773eee07c25a907e96ca7e054ac58
Arazo_Leo <151800577+Arazoleo@users.noreply.github.com>
2024-09-16 13:37:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function windowTravel(id){
+    window.location.href = id;
+    }
+    
+    function activateAnimation() {
+        document.querySelector('.container').classList.add('active');
+      }
+
+      function activateCont2() {
+        document.querySelector('.containerOR').classList.add('active');
+      }
+      function activateCont3(){
+        document.querySelector('.containerNot').classList.add('active');
+      }
+      function activateCont4(){
+        document.querySelector('.containerXOR').classList.add('active');
+      }
+      function activateCont5(){
+        document.querySelector('.containerXNOR').classList.add('active');
+      }
+      function activateCont6(){
+        document.querySelector('.containerNAND').classList.add('active');
+      }
+      function activateCont7(){
+        document.querySelector('.containerNOR').classList.add('active');
+      }
+      function activateCont8() {
+        document.querySelector('.containerD').classList.add('active');
+      }
+      function activateCont9(){
+        document.querySelector('.containerT').classList.add('active');
+      }
+      function activateCont10(){
+        document.querySelector('.containerSR').classList.add('active');
+      }
+      function activateCont11(){
+        document.querySelector('.containerJK').classList.add('active');
+      }
+      
